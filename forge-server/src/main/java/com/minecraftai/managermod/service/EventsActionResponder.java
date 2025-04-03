9f7dee73334399c3701b0da8f17020e6a7d5f925
artemulianko <a.ulianko@insart.com>
2025-04-03 12:22:15 +0200

    Add Dagger 2 DI support to ManagerMod
    Integrated Dagger 2 for dependency injection to simplify object management and improve maintainability. Refactored key components and handlers to utilize DI, including ActionsProcessor, EventTracker, and ServerEventsHandler. Introduced DIContainer, AppModule, and ServerHolder for managing dependencies.

--- forge-server/src/main/java/com/minecraftai/managermod/service/EventsActionResponder.java
+++ forge-server/src/main/java/com/minecraftai/managermod/service/EventsActionResponder.java
@@ -5,13 +5,21 @@ import com.minecraftai.managermod.actions.AbstractAction;
 import com.minecraftai.managermod.actions.SpawnCow;
 import com.minecraftai.managermod.config.JsonConfig;
 import com.minecraftai.managermod.events.AbstractGameEvent;
+import jakarta.inject.Inject;
 
 import java.util.List;
 
 public class EventsActionResponder {
     private final static ObjectMapper objectMapper = JsonConfig.createObjectMapper();
 
-    public static List<AbstractAction> respond(List<AbstractGameEvent> events) {
+    @Inject
+    public EventsActionResponder() {}
+
+    /**
+     * Processes a list of game events and generates a corresponding list of actions.
+     * If the event list is empty, no actions are returned.
+     */
+    public List<AbstractAction> respond(List<AbstractGameEvent> events) {
         try {
             String eventsJson = objectMapper.writeValueAsString(events);
             System.out.println(eventsJson);
